91eb0137f7110fd390b7fdf7e00038361c361cd4
LordOfTrident <lordoftrident@gmail.com>
2024-03-03 00:23:12 +0100

    Add more regex functions

--- regex.go
+++ regex.go
@@ -13,3 +13,15 @@ func Strip(text string) string {
 func FindNext(text string) []int {
 	return Regex.FindStringIndex(text)
 }
+
+func Count(text string) int {
+	return len(Regex.FindAllString(text, -1))
+}
+
+func LengthNoAnsi(text string) int {
+	return len(text) - Count(text)
+}
+
+func Contains(text string) bool {
+	return Regex.MatchString(text)
+}
